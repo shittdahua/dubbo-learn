f496d759ca0e87663eb6a9d03afd6ca9700db181
STONE <tt2stone@163.com>
2019-02-24 20:01:25 +0800

    init

---
+++ tt-consumer/src/main/java/com/dubbo/consumer/ttconsumer/TtConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dubbo.consumer.ttconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtConsumerApplication.class, args);
+	}
+
+}
---
+++ tt-provider/src/main/java/com/dubbo/provider/ttprovider/TtProviderApplication.java
@@ -0,0 +1,13 @@
+package com.dubbo.provider.ttprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtProviderApplication.class, args);
+	}
+
+}
